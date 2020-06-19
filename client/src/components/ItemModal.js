457c1d0b8ea42c9c2f33cb46b1bc84429ba45932
Allen Hu <allensamuelhu@gmail.com>
2020-06-18 21:05:19 -0400

    Connected Front and Back End
    Ready to deploy

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -11,7 +11,6 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import { v4 as uuid } from 'uuid';
 import PropTypes from 'prop-types';
 
 class ItemModal extends Component {
@@ -34,7 +33,6 @@ class ItemModal extends Component {
         e.preventDefault();
 
         const newItem = {
-            id: uuid(),
             name: this.state.name
         }
 
