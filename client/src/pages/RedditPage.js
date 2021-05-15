cf96a0f64e4a10971fe7f4053a54f8b6aea9a899
Allen Hu <allensamuelhu@gmail.com>
2021-05-15 10:22:49 -0400

    Get DevExtreme to work with dependencies

--- client/src/pages/RedditPage.js
+++ client/src/pages/RedditPage.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import {
+  Chart,
+  BarSeries,
+  Title,
+  ArgumentAxis,
+  ValueAxis,
+} from '@devexpress/dx-react-chart-material-ui';
+
+import { Animation } from '@devexpress/dx-react-chart';
+
+const data = [
+    { year: '1950', population: 2.525 },
+    { year: '1960', population: 3.018 },
+    { year: '1970', population: 3.682 },
+    { year: '1980', population: 4.440 },
+    { year: '1990', population: 5.310 },
+    { year: '2000', population: 6.127 },
+    { year: '2010', population: 6.930 },
+  ];
 
 const RedditPage = () => (   
     <Container>
@@ -21,7 +41,21 @@ const RedditPage = () => (
         <Typography variant="body1">
             This is a bar chart of the hours vs # of comments in the subreddit.
         </Typography>
-        
+        <Paper>
+            <Chart
+            data={data}
+            >
+            <ArgumentAxis />
+            <ValueAxis />
+
+            <BarSeries
+                valueField="population"
+                argumentField="year"
+            />
+            <Title text="World population" />
+            <Animation />
+            </Chart>
+        </Paper>
         <br/>
         <Typography variant="h5">
             Sentiment Analysis
